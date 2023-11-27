102a449dea541e4a05a2a855fa6a5e8d75246298
aydnOktay <xaydinoktay@gmail.com>
2023-11-27 16:07:25 +0300

    frondend

--- src/app/app.controller.ts
+++ src/app/app.controller.ts
@@ -18,4 +18,19 @@ export class AppController {
     async myProjects(@Res() res: Response) {
         res.render("myprojects")
     }
+
+    @Get("/members")
+    async members(@Res() res: Response) {
+        res.render("members")
+    }
+
+    @Get("/profil")
+    async profile(@Res() res: Response) {
+        res.render("profile")
+    }
+    
+    @Get("/projectDetail")
+    async projectDetail(@Res() res: Response) {
+        res.render("projectDetail")
+    }
 }
